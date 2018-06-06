3aca5dbebd65864cc8cbd8fc0a29cb6d15e2c41e
SamsonWang <wangzhilv@gmail.com>
2018-06-06 13:51:46 +0800

    update shm attach

--- example/shm_attach.cpp
+++ example/shm_attach.cpp
@@ -1,14 +1,26 @@
 
-
 #include <cstdio>
+#include "SharedMemKeyGen.h"
 #include "SharedMemOper.h"
+
+#define UNUSED_ARGS(x)     (void)x
 
 int main(int argc, char *argv[])
 {
+    UNUSED_ARGS(argc);
+    UNUSED_ARGS(argv);
+
     printf("example: shm attach\n");
 
+    CSharedMemKeyGen objKeyGen;
+    objKeyGen.SetParam("./example_shmkey", 1);
+    objKeyGen.GenerateKey();
     
-    
+
+    CSharedMemOper objShmOper;
+    objShmOper.SetSharedMemKey(objKeyGen.GetKey());
+    objShmOper.AttachSharedMem();
+
     getchar();
     return 0;
 }
--- example/shm_create.cpp
+++ example/shm_create.cpp
@@ -1,12 +1,16 @@
 
-
 #include <cstdio>
 #include "SharedMemKeyGen.h"
 #include "SharedMemOper.h"
 
 
+#define UNUSED_ARGS(x)     (void)x
+
 int main(int argc, char *argv[])
 {
+    UNUSED_ARGS(argc);
+    UNUSED_ARGS(argv);
+    
     printf("example: shm create\n");
 
     CSharedMemKeyGen objKeyGen;
@@ -17,8 +21,7 @@ int main(int argc, char *argv[])
     CSharedMemOper objShmOper;
     objShmOper.SetSharedMemKey(objKeyGen.GetKey());
     objShmOper.CreateSharedMem(200);
-    
-    
+
     getchar();
     return 0;
 }
